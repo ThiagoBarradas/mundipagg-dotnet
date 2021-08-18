a6cc6fd59cd1ad22f1899d4332e628ce45c03b3d
Hermano Lourenco Souza Lustosa <hermano.lustosa@mundipagg.com>
2021-08-18 15:17:25 -0300

    :sparkles: Adds merchant async methods

--- Mundipagg/Resources/Interface/IMerchantResource.cs
+++ Mundipagg/Resources/Interface/IMerchantResource.cs
@@ -1,6 +1,7 @@
 ﻿using Mundipagg.Models.Request;
 using Mundipagg.Models.Response;
 using RestSharp.Easy.Models;
+using System.Threading.Tasks;
 
 namespace Mundipagg.Resources.Interface
 {
@@ -10,11 +11,18 @@ namespace Mundipagg.Resources.Interface
     public interface IMerchantResource : IResource
     {
         /// <summary>
-        /// Get merchant info
+        /// Cretes a new merchant
         /// </summary>
         /// <param name="merchantId"></param>
         /// <returns></returns>
         BaseResponse<GetMerchantResponse, MundipaggErrorsResponse> GetMerchant(string merchantId);
+
+        /// <summary>
+        /// Cretes a new merchant
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        Task<BaseResponse<GetMerchantResponse, MundipaggErrorsResponse>> GetMerchantAsync(string merchantId);
 
         /// <summary>
         /// Cretes a new merchant
@@ -28,7 +36,21 @@ namespace Mundipagg.Resources.Interface
         /// </summary>
         /// <param name="request"></param>
         /// <returns></returns>
+        Task<BaseResponse<GetMerchantResponse, MundipaggErrorsResponse>> CreateMerchantAsync(CreateMerchantRequest request);
+
+        /// <summary>
+        /// Update a merchant
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
         BaseResponse<GetMerchantResponse, MundipaggErrorsResponse> UpdateMerchant(string merchantId, UpdateMerchantRequest request);
+
+        /// <summary>
+        /// Update status of a merchant
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        Task<BaseResponse<GetMerchantResponse, MundipaggErrorsResponse>> UpdateMerchantAsync(string merchantId, UpdateMerchantRequest request);
 
         /// <summary>
         /// Update status of a merchant
@@ -43,6 +65,22 @@ namespace Mundipagg.Resources.Interface
         /// <param name="merchantId"></param>
         /// <param name="request"></param>
         /// <returns></returns>
+        Task<BaseResponse<GetMerchantResponse, MundipaggErrorsResponse>> UpdateMerchantStatusAsync(string merchantId, UpdateMerchantStatusRequest request);
+
+        /// <summary>
+        /// Update settings of a Merchant
+        /// </summary>
+        /// <param name="merchantId"></param>
+        /// <param name="request"></param>
+        /// <returns></returns>
         BaseResponse<GetMerchantResponse, MundipaggErrorsResponse> UpdateMerchantSettings(string merchantId, UpdateMerchantSettingsRequest request);
+
+        /// <summary>
+        /// Update settings of a Merchant
+        /// </summary>
+        /// <param name="merchantId"></param>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        Task<BaseResponse<GetMerchantResponse, MundipaggErrorsResponse>> UpdateMerchantSettingsAsync(string merchantId, UpdateMerchantSettingsRequest request);
     }
 }
--- Mundipagg/Resources/MerchantResource.cs
+++ Mundipagg/Resources/MerchantResource.cs
@@ -3,6 +3,7 @@ using Mundipagg.Models.Response;
 using Mundipagg.Resources.Interface;
 using RestSharp.Easy.Models;
 using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace Mundipagg.Resources
 {
@@ -20,6 +21,16 @@ namespace Mundipagg.Resources
             return this.SendRequest<GetMerchantResponse>(method, endpoint, null, authMode: "token");
         }
 
+
+        public async Task<BaseResponse<GetMerchantResponse, MundipaggErrorsResponse>> GetMerchantAsync(string merchantId)
+        {
+            var method = HttpMethod.Get;
+            var endpoint = $"/merchants/{merchantId}";
+
+            return await this.SendRequestAsync<GetMerchantResponse>(method, endpoint, null, authMode: "token");
+        }
+
+
         public BaseResponse<GetMerchantResponse, MundipaggErrorsResponse> CreateMerchant(CreateMerchantRequest request)
         {
             var method = HttpMethod.Post;
@@ -27,6 +38,16 @@ namespace Mundipagg.Resources
 
             return this.SendRequest<GetMerchantResponse>(method, endpoint, request, authMode: "token");
         }
+
+
+        public async Task<BaseResponse<GetMerchantResponse, MundipaggErrorsResponse>> CreateMerchantAsync(CreateMerchantRequest request)
+        {
+            var method = HttpMethod.Post;
+            var endpoint = $"/merchants";
+
+            return await this.SendRequestAsync<GetMerchantResponse>(method, endpoint, request, authMode: "token");
+        }
+
 
         public BaseResponse<GetMerchantResponse, MundipaggErrorsResponse> UpdateMerchant(string merchantId, UpdateMerchantRequest request)
         {
@@ -36,6 +57,16 @@ namespace Mundipagg.Resources
             return this.SendRequest<GetMerchantResponse>(method, endpoint, request, authMode: "token");
         }
 
+
+        public async Task<BaseResponse<GetMerchantResponse, MundipaggErrorsResponse>> UpdateMerchantAsync(string merchantId, UpdateMerchantRequest request)
+        {
+            var method = HttpMethod.Put;
+            var endpoint = $"/merchants/{merchantId}";
+
+            return await this.SendRequestAsync<GetMerchantResponse>(method, endpoint, request, authMode: "token");
+        }
+
+
         public BaseResponse<GetMerchantResponse, MundipaggErrorsResponse> UpdateMerchantStatus(string merchantId, UpdateMerchantStatusRequest request)
         {
             var method = new HttpMethod("patch");
@@ -44,12 +75,31 @@ namespace Mundipagg.Resources
             return this.SendRequest<GetMerchantResponse>(method, endpoint, request, authMode: "token");
         }
 
+
+        public async Task<BaseResponse<GetMerchantResponse, MundipaggErrorsResponse>> UpdateMerchantStatusAsync(string merchantId, UpdateMerchantStatusRequest request)
+        {
+            var method = new HttpMethod("patch");
+            var endpoint = $"/merchants/{merchantId}/status";
+
+            return await this.SendRequestAsync<GetMerchantResponse>(method, endpoint, request, authMode: "token");
+        }
+
+
         public BaseResponse<GetMerchantResponse, MundipaggErrorsResponse> UpdateMerchantSettings(string merchantId, UpdateMerchantSettingsRequest request)
         {
             var method = new HttpMethod("patch");
             var endpoint = $"/merchants/{merchantId}/merchant-settings";
 
             return this.SendRequest<GetMerchantResponse>(method, endpoint, request, authMode: "token");
+        }
+
+
+        public async Task<BaseResponse<GetMerchantResponse, MundipaggErrorsResponse>> UpdateMerchantSettingsAsync(string merchantId, UpdateMerchantSettingsRequest request)
+        {
+            var method = new HttpMethod("patch");
+            var endpoint = $"/merchants/{merchantId}/merchant-settings";
+
+            return await this.SendRequestAsync<GetMerchantResponse>(method, endpoint, request, authMode: "token");
         }
     }
 }
