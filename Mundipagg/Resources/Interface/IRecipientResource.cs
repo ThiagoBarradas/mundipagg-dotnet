3fcc4d7ef5f4486422712ea5b9cc7af7c2ac1e61
Hermano Lourenco Souza Lustosa <hermano.lustosa@mundipagg.com>
2021-08-18 15:40:08 -0300

    :sparkles: Adds recipient async methods

--- Mundipagg/Resources/Interface/IRecipientResource.cs
+++ Mundipagg/Resources/Interface/IRecipientResource.cs
@@ -2,6 +2,7 @@
 using Mundipagg.Models.Request;
 using Mundipagg.Models.Response;
 using RestSharp.Easy.Models;
+using System.Threading.Tasks;
 
 namespace Mundipagg.Resources.Interface
 {
@@ -11,24 +12,38 @@ namespace Mundipagg.Resources.Interface
     public interface IRecipientResource : IResource
     {
         /// <summary>
-        /// Gets a recipient
+        /// Create a recipient
         /// </summary>
-        /// <param name="recipientId">Required parameter: recipient id</param>
-        /// <return>Returns BaseResponse<GetRecipientResponse> response from the API call</return>
+        /// <param name="recipientId"></param>
+        /// <returns></returns>
         BaseResponse<GetRecipientResponse, MundipaggErrorsResponse> GetRecipient(string recipientId);
 
         /// <summary>
         /// Create a recipient
         /// </summary>
         /// <param name="request"></param>
+        Task<BaseResponse<GetRecipientResponse, MundipaggErrorsResponse>> GetRecipientAsync(string recipientId);
+
+        /// <summary>
+        /// Create a recipient
+        /// </summary>
+        /// <param name="request"></param>
         /// <return>Returns BaseResponse<GetRecipientResponse> response from the API call</return>
+        /// <return>Returns Task<BaseResponse<GetRecipientResponse>> response from the API call</return>
         BaseResponse<GetRecipientResponse, MundipaggErrorsResponse> CreateRecipient(CreateRecipientRequest request);
 
         /// <summary>
         /// Update a recipient
         /// </summary>
         /// <param name="request"></param>
+        Task<BaseResponse<GetRecipientResponse, MundipaggErrorsResponse>> CreateRecipientAsync(CreateRecipientRequest request);
+
+        /// <summary>
+        /// Update a recipient
+        /// </summary>
+        /// <param name="request"></param>
         /// <return>Returns BaseResponse<GetRecipientResponse> response from the API call</return>
+        /// <return>Returns Task<BaseResponse<GetRecipientResponse>> response from the API call</return>
         BaseResponse<GetRecipientResponse, MundipaggErrorsResponse> UpdateRecipient(string recipientId, UpdateRecipientRequest request);
 
         /// <summary>
@@ -36,7 +51,15 @@ namespace Mundipagg.Resources.Interface
         /// </summary>
         /// <param name="recipientId"></param>
         /// <param name="request"></param>
+        Task<BaseResponse<GetRecipientResponse, MundipaggErrorsResponse>> UpdateRecipientAsync(string recipientId, UpdateRecipientRequest request);
+
+        /// <summary>
+        /// Update a recipient's transfer settings
+        /// </summary>
+        /// <param name="recipientId"></param>
+        /// <param name="request"></param>
         /// <return>Returns BaseResponse<GetRecipientResponse> response from the API call</return>
+        /// <return>Returns Task<BaseResponse<GetRecipientResponse>> response from the API call</return>
         BaseResponse<GetRecipientResponse, MundipaggErrorsResponse> UpdateTransferSettings(string recipientId, UpdateTransferSettingsRequest request);
 
         /// <summary>
@@ -44,7 +67,15 @@ namespace Mundipagg.Resources.Interface
         /// </summary>
         /// <param name="recipientId"></param>
         /// <param name="request"></param>
+        Task<BaseResponse<GetRecipientResponse, MundipaggErrorsResponse>> UpdateTransferSettingsAsync(string recipientId, UpdateTransferSettingsRequest request);
+
+        /// <summary>
+        /// Update a recipient's automatic anticipation settings
+        /// </summary>
+        /// <param name="recipientId"></param>
+        /// <param name="request"></param>
         /// <return>Returns BaseResponse<GetRecipientResponse> response from the API call</return>
+        /// <return>Returns Task<BaseResponse<GetRecipientResponse>> response from the API call</return>
         BaseResponse<GetRecipientResponse, MundipaggErrorsResponse> UpdateAutomaticAnticipationSettings(string recipientId, UpdateAutomaticAnticipationSettingsRequest request);
 
         /// <summary>
@@ -52,7 +83,15 @@ namespace Mundipagg.Resources.Interface
         /// </summary>
         /// <param name="recipientId"></param>
         /// <param name="request"></param>
+        Task<BaseResponse<GetRecipientResponse, MundipaggErrorsResponse>> UpdateAutomaticAnticipationSettingsAsync(string recipientId, UpdateAutomaticAnticipationSettingsRequest request);
+
+        /// <summary>
+        /// Update a recipient's default bank account
+        /// </summary>
+        /// <param name="recipientId"></param>
+        /// <param name="request"></param>
         /// <return>Returns BaseResponse<GetRecipientResponse> response from the API call</return>
+        /// <return>Returns Task<BaseResponse<GetRecipientResponse>> response from the API call</return>
         BaseResponse<GetRecipientResponse, MundipaggErrorsResponse> UpdateDefaultBankAccount(string recipientId, UpdateRecipientDefaultBankAccountRequest request);
 
 
@@ -60,21 +99,50 @@ namespace Mundipagg.Resources.Interface
         /// List recipients
         /// </summary>
         /// <param name="request"></param>
+        Task<BaseResponse<GetRecipientResponse, MundipaggErrorsResponse>> UpdateDefaultBankAccountAsync(string recipientId, UpdateRecipientDefaultBankAccountRequest request);
+
+
+        /// <summary>
+        /// List recipients
+        /// </summary>
+        /// <param name="request"></param>
         /// <returns>Returns BaseResponse<PagingResponse<GetRecipientResponse>> response from the API call</returns>
+        /// <returns>Returns Task<BaseResponse<PagingResponse<GetRecipientResponse>>> response from the API call</returns>
         BaseResponse<PagingResponse<GetRecipientResponse>, MundipaggErrorsResponse> ListRecipients(ListRecipientsRequest request);
 
         /// <summary>
         /// Import a recipient
         /// </summary>
         /// <param name="request"></param>
+        Task<BaseResponse<PagingResponse<GetRecipientResponse>, MundipaggErrorsResponse>> ListRecipientsAsync(ListRecipientsRequest request);
+
+        /// <summary>
+        /// Import a recipient
+        /// </summary>
+        /// <param name="request"></param>
         /// <returns>Returns BaseResponse<GetRecipientResponse> response from the API call</returns>
+        /// <returns>Returns Task<BaseResponse<GetRecipientResponse>> response from the API call</returns>
         BaseResponse<GetRecipientResponse, MundipaggErrorsResponse> ImportRecipient(CreateImportRecipientRequest request);
 
         /// <summary>
         /// Gets a recipient by code
         /// </summary>
         /// <param name="code"></param>
+        Task<BaseResponse<GetRecipientResponse, MundipaggErrorsResponse>> ImportRecipientAsync(CreateImportRecipientRequest request);
+
+        /// <summary>
+        /// Gets a recipient by code
+        /// </summary>
+        /// <param name="code"></param>
         /// <returns>Returns BaseResponse<GetRecipientResponse> response from the API call</returns>
+        /// <returns>Returns Task<BaseResponse<GetRecipientResponse>> response from the API call</returns>
         BaseResponse<GetRecipientResponse, MundipaggErrorsResponse> GetRecipientByCode(string code);
+
+        /// <summary>
+        /// Gets a recipient by code
+        /// </summary>
+        /// <param name="code"></param>
+        /// <returns></returns>
+        Task<BaseResponse<GetRecipientResponse, MundipaggErrorsResponse>> GetRecipientByCodeAsync(string code);
     }
 }
--- Mundipagg/Resources/RecipientResource.cs
+++ Mundipagg/Resources/RecipientResource.cs
@@ -5,6 +5,7 @@ using Mundipagg.Resources.Interface;
 using Mundipagg.Utils;
 using RestSharp.Easy.Models;
 using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace Mundipagg.Resources
 {
@@ -20,6 +21,16 @@ namespace Mundipagg.Resources
             return this.SendRequest<GetRecipientResponse>(method, endpoint, request);
         }
 
+
+        public async Task<BaseResponse<GetRecipientResponse, MundipaggErrorsResponse>> CreateRecipientAsync(CreateRecipientRequest request)
+        {
+            var method = HttpMethod.Post;
+            var endpoint = $"/recipients";
+
+            return await this.SendRequestAsync<GetRecipientResponse>(method, endpoint, request);
+        }
+
+
         public BaseResponse<GetRecipientResponse, MundipaggErrorsResponse> GetRecipient(string recipientId)
         {
             var method = HttpMethod.Get;
@@ -27,6 +38,16 @@ namespace Mundipagg.Resources
 
             return this.SendRequest<GetRecipientResponse>(method, endpoint, null);
         }
+
+
+        public async Task<BaseResponse<GetRecipientResponse, MundipaggErrorsResponse>> GetRecipientAsync(string recipientId)
+        {
+            var method = HttpMethod.Get;
+            var endpoint = $"/recipients/{recipientId}";
+
+            return await this.SendRequestAsync<GetRecipientResponse>(method, endpoint, null);
+        }
+
 
         public BaseResponse<PagingResponse<GetRecipientResponse>, MundipaggErrorsResponse> ListRecipients(ListRecipientsRequest request)
         {
@@ -37,6 +58,17 @@ namespace Mundipagg.Resources
             return this.SendRequest<PagingResponse<GetRecipientResponse>>(method, endpoint, null, query);
         }
 
+
+        public async Task<BaseResponse<PagingResponse<GetRecipientResponse>, MundipaggErrorsResponse>> ListRecipientsAsync(ListRecipientsRequest request)
+        {
+            var method = HttpMethod.Get;
+            var endpoint = $"/recipients";
+            var query = request.ToDictionary();
+
+            return await this.SendRequestAsync<PagingResponse<GetRecipientResponse>>(method, endpoint, null, query);
+        }
+
+
         public BaseResponse<GetRecipientResponse, MundipaggErrorsResponse> UpdateRecipient(string recipientId, UpdateRecipientRequest request)
         {
             var method = HttpMethod.Put;
@@ -44,6 +76,16 @@ namespace Mundipagg.Resources
 
             return this.SendRequest<GetRecipientResponse>(method, endpoint, request);
         }
+
+
+        public async Task<BaseResponse<GetRecipientResponse, MundipaggErrorsResponse>> UpdateRecipientAsync(string recipientId, UpdateRecipientRequest request)
+        {
+            var method = HttpMethod.Put;
+            var endpoint = $"/recipients/{recipientId}";
+
+            return await this.SendRequestAsync<GetRecipientResponse>(method, endpoint, request);
+        }
+
 
         public BaseResponse<GetRecipientResponse, MundipaggErrorsResponse> UpdateTransferSettings(string recipientId, UpdateTransferSettingsRequest request)
         {
@@ -53,6 +95,16 @@ namespace Mundipagg.Resources
             return this.SendRequest<GetRecipientResponse>(method, endpoint, request);
         }
 
+
+        public async Task<BaseResponse<GetRecipientResponse, MundipaggErrorsResponse>> UpdateTransferSettingsAsync(string recipientId, UpdateTransferSettingsRequest request)
+        {
+            var method = new HttpMethod("patch");
+            var endpoint = $"/recipients/{recipientId}/transfer-settings";
+
+            return await this.SendRequestAsync<GetRecipientResponse>(method, endpoint, request);
+        }
+
+
         public BaseResponse<GetRecipientResponse, MundipaggErrorsResponse> UpdateAutomaticAnticipationSettings(string recipientId, UpdateAutomaticAnticipationSettingsRequest request)
         {
             var method = new HttpMethod("patch");
@@ -60,6 +112,16 @@ namespace Mundipagg.Resources
 
             return this.SendRequest<GetRecipientResponse>(method, endpoint, request);
         }
+
+
+        public async Task<BaseResponse<GetRecipientResponse, MundipaggErrorsResponse>> UpdateAutomaticAnticipationSettingsAsync(string recipientId, UpdateAutomaticAnticipationSettingsRequest request)
+        {
+            var method = new HttpMethod("patch");
+            var endpoint = $"/recipients/{recipientId}/automatic-anticipation-settings";
+
+            return await this.SendRequestAsync<GetRecipientResponse>(method, endpoint, request);
+        }
+
 
         public BaseResponse<GetRecipientResponse, MundipaggErrorsResponse> UpdateDefaultBankAccount(string recipientId, UpdateRecipientDefaultBankAccountRequest request)
         {
@@ -69,6 +131,16 @@ namespace Mundipagg.Resources
             return this.SendRequest<GetRecipientResponse>(method, endpoint, request);
         }
 
+
+        public async Task<BaseResponse<GetRecipientResponse, MundipaggErrorsResponse>> UpdateDefaultBankAccountAsync(string recipientId, UpdateRecipientDefaultBankAccountRequest request)
+        {
+            var method = new HttpMethod("patch");
+            var endpoint = $"/recipients/{recipientId}/default-bank-account";
+
+            return await this.SendRequestAsync<GetRecipientResponse>(method, endpoint, request);
+        }
+
+
         public BaseResponse<GetRecipientResponse, MundipaggErrorsResponse> ImportRecipient(CreateImportRecipientRequest request)
         {
             var method = HttpMethod.Post;
@@ -77,12 +149,31 @@ namespace Mundipagg.Resources
             return this.SendRequest<GetRecipientResponse>(method, endpoint, request);
         }
 
+
+        public async Task<BaseResponse<GetRecipientResponse, MundipaggErrorsResponse>> ImportRecipientAsync(CreateImportRecipientRequest request)
+        {
+            var method = HttpMethod.Post;
+            var endpoint = $"/recipients/import";
+
+            return await this.SendRequestAsync<GetRecipientResponse>(method, endpoint, request);
+        }
+
+
         public BaseResponse<GetRecipientResponse, MundipaggErrorsResponse> GetRecipientByCode(string code)
         {
             var method = HttpMethod.Get;
             var endpoint = $"/recipients/code/{code}";
 
             return this.SendRequest<GetRecipientResponse>(method, endpoint, null);
+        }
+
+
+        public async Task<BaseResponse<GetRecipientResponse, MundipaggErrorsResponse>> GetRecipientByCodeAsync(string code)
+        {
+            var method = HttpMethod.Get;
+            var endpoint = $"/recipients/code/{code}";
+
+            return await this.SendRequestAsync<GetRecipientResponse>(method, endpoint, null);
         }
     }
 }
