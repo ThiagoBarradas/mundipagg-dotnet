43a84eb1cc1f2bcea0f10878a7a4a073b628f36b
Hermano Lourenco Souza Lustosa <hermano.lustosa@mundipagg.com>
2021-08-18 15:31:10 -0300

    :sparkles: Adds plans async methods

--- Mundipagg/Resources/Interface/IPlanResource.cs
+++ Mundipagg/Resources/Interface/IPlanResource.cs
@@ -2,6 +2,7 @@
 using Mundipagg.Models.Request;
 using Mundipagg.Models.Response;
 using RestSharp.Easy.Models;
+using System.Threading.Tasks;
 
 namespace Mundipagg.Resources.Interface
 {
@@ -11,16 +12,29 @@ namespace Mundipagg.Resources.Interface
     public interface IPlanResource : IResource
     {
         /// <summary>
-        /// Gets a plan
+        /// Lists all plans
         /// </summary>
-        /// <param name="planId">Required parameter: Plan id</param>
-        /// <return>Returns BaseResponse<GetPlanResponse> response from the API call</return>
+        /// <param name="planId"></param>
+        /// <returns></returns>
         BaseResponse<GetPlanResponse, MundipaggErrorsResponse> GetPlan(string planId);
 
         /// <summary>
         /// Lists all plans
         /// </summary>
+        Task<BaseResponse<GetPlanResponse, MundipaggErrorsResponse>> GetPlanAsync(string planId);
+
+        /// <summary>
+        /// Lists all plans
+        /// </summary>
         /// <return>Returns BaseResponse<PagingResponse<GetPlanResponse>> response from the API call</return>
+        /// <return>Returns Task<BaseResponse<PagingResponse<GetPlanResponse>>> response from the API call</return>
         BaseResponse<PagingResponse<GetPlanResponse>, MundipaggErrorsResponse> ListPlans(ListPlansRequest request);
+
+        /// <summary>
+        /// Lists all plans
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        Task<BaseResponse<PagingResponse<GetPlanResponse>, MundipaggErrorsResponse>> ListPlansAsync(ListPlansRequest request);
     }
 }
