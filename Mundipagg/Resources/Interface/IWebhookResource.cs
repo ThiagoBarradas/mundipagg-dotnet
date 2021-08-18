d94f4a09f272156e23e870ec57aae8d89cb1200d
Hermano Lourenco Souza Lustosa <hermano.lustosa@mundipagg.com>
2021-08-18 15:44:13 -0300

    :sparkles: Adds webhook async methods

--- Mundipagg/Resources/Interface/IWebhookResource.cs
+++ Mundipagg/Resources/Interface/IWebhookResource.cs
@@ -1,8 +1,8 @@
 ﻿using Mundipagg.Models.Commons;
 using Mundipagg.Models.Request;
 using Mundipagg.Models.Response;
-using Mundipagg.Models.Webhooks;
 using RestSharp.Easy.Models;
+using System.Threading.Tasks;
 
 namespace Mundipagg.Resources.Interface
 {
@@ -12,18 +12,18 @@ namespace Mundipagg.Resources.Interface
     public interface IWebhookResource : IResource
     {
         /// <summary>
-        /// Parse webhook received from Mundipagg Api
+        /// Get webhook data
         /// </summary>
-        /// <param name="json">Received json</param>
-        /// <returns>Webhook object</returns>
-        WebhookReceived ParseWebhook(string json);
+        /// <param name="request"></param>
+        /// <returns></returns>
+        BaseResponse<PagingResponse<GetWebhookResponse>, MundipaggErrorsResponse> ListWebhooks(ListWebhooksRequest request);
 
         /// <summary>
-        /// List webhooks
+        /// Get webhook data
         /// </summary>
-        /// <param name="request">List webhooks request</param>
-        /// <returns>Base response with paged webhooks data</returns>
-        BaseResponse<PagingResponse<GetWebhookResponse>, MundipaggErrorsResponse> ListWebhooks(ListWebhooksRequest request);
+        /// <param name="webhookId">Webhook id</param>
+        /// <returns>Base response with webhook data</returns>
+        Task<BaseResponse<PagingResponse<GetWebhookResponse>, MundipaggErrorsResponse>> ListWebhooksAsync(ListWebhooksRequest request);
 
         /// <summary>
         /// Get webhook data
@@ -37,6 +37,20 @@ namespace Mundipagg.Resources.Interface
         /// </summary>
         /// <param name="webhookId">Webhook id</param>
         /// <returns>Base response with webhook data</returns>
+        Task<BaseResponse<GetWebhookResponse, MundipaggErrorsResponse>> GetWebhookAsync(string webhookId);
+
+        /// <summary>
+        /// Retry send webhook
+        /// </summary>
+        /// <param name="webhookId">Webhook id</param>
+        /// <returns>Base response with webhook data</returns>
         BaseResponse<GetWebhookResponse, MundipaggErrorsResponse> RetryWebhook(string webhookId);
+
+        /// <summary>
+        /// Retry send webhook
+        /// </summary>
+        /// <param name="webhookId"></param>
+        /// <returns></returns>
+        Task<BaseResponse<GetWebhookResponse, MundipaggErrorsResponse>> RetryWebhookAsync(string webhookId);
     }
 }
--- Mundipagg/Resources/WebhookResource.cs
+++ Mundipagg/Resources/WebhookResource.cs
@@ -8,12 +8,14 @@ using Newtonsoft.Json;
 using RestSharp.Easy.Models;
 using System;
 using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace Mundipagg.Resources
 {
     public class WebhookResource : BaseResource, IWebhookResource
     {
         public WebhookResource(Configuration configuration) : base(configuration) { }
+
 
         public BaseResponse<GetWebhookResponse, MundipaggErrorsResponse> GetWebhook(string webhookId)
         {
@@ -23,6 +25,16 @@ namespace Mundipagg.Resources
             return this.SendRequest<GetWebhookResponse>(method, endpoint, null);
         }
 
+
+        public async Task<BaseResponse<GetWebhookResponse, MundipaggErrorsResponse>> GetWebhookAsync(string webhookId)
+        {
+            var method = HttpMethod.Get;
+            var endpoint = $"/hooks/{webhookId}";
+
+            return await this.SendRequestAsync<GetWebhookResponse>(method, endpoint, null);
+        }
+
+
         public BaseResponse<PagingResponse<GetWebhookResponse>, MundipaggErrorsResponse> ListWebhooks(ListWebhooksRequest request)
         {
             var method = HttpMethod.Get;
@@ -31,7 +43,7 @@ namespace Mundipagg.Resources
 
             return this.SendRequest<PagingResponse<GetWebhookResponse>>(method, endpoint, null, query);
         }
-        
+
         public WebhookReceived ParseWebhook(string json)
         {
             if (string.IsNullOrWhiteSpace(json) == true)
@@ -40,12 +52,30 @@ namespace Mundipagg.Resources
             return JsonConvert.DeserializeObject<WebhookReceived>(json, JsonSerializerUtil.SnakeCaseSettings);
         }
 
+        public async Task<BaseResponse<PagingResponse<GetWebhookResponse>, MundipaggErrorsResponse>> ListWebhooksAsync(ListWebhooksRequest request)
+        {
+            var method = HttpMethod.Get;
+            var endpoint = $"/hooks";
+            var query = request.ToDictionary();
+
+            return await this.SendRequestAsync<PagingResponse<GetWebhookResponse>>(method, endpoint, null, query);
+        }
+
         public BaseResponse<GetWebhookResponse, MundipaggErrorsResponse> RetryWebhook(string webhookId)
         {
             var method = HttpMethod.Post;
             var endpoint = $"/hooks/{webhookId}/retry";
 
             return this.SendRequest<GetWebhookResponse>(method, endpoint, null);
+        }
+
+
+        public async Task<BaseResponse<GetWebhookResponse, MundipaggErrorsResponse>> RetryWebhookAsync(string webhookId)
+        {
+            var method = HttpMethod.Post;
+            var endpoint = $"/hooks/{webhookId}/retry";
+
+            return await this.SendRequestAsync<GetWebhookResponse>(method, endpoint, null);
         }
     }
 }
