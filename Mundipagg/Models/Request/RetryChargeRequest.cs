59de668957fc8eadd8430f4e0d544eb8517d9afe
Gustavo Jordão | Stone <gustavo.jordao@pagar.me>
2022-03-08 10:50:36 -0300

    Add charge retry endpoint with configuration

---
+++ Mundipagg/Models/Request/RetryChargeRequest.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Mundipagg.Models.Request
+{
+    [JsonObject(NamingStrategyType = typeof(SnakeCaseNamingStrategy))]
+    public class RetryChargeRequest
+    {
+        public bool Capture { get; set; }
+
+        public bool AntifraudEnabled { get; set; }
+    }
+}
