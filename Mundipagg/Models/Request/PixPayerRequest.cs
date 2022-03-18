68189ec13cf0ef044464e0d2e6b4c753d43b2b96
Lucas Michael <lucas.mssantos@pagar.me>
2022-03-18 14:17:04 -0300

    ✨ Adds EndToEndId and Payer properties into CreatePixPaymentRequest

---
+++ Mundipagg/Models/Request/PixPayerRequest.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Mundipagg.Models.Request
+{
+    [JsonObject(NamingStrategyType = typeof(SnakeCaseNamingStrategy))]
+    public class PixPayerRequest
+    {
+        public string Name { get; set; }
+
+        public string Document { get; set; }
+
+        public string DocumentType { get; set; }
+
+        public PixBankAccountRequest BankAccount { get; set; }
+    }
+}
