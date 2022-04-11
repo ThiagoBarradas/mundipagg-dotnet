63fba612ee4057c2595fb1b250196484be11c4b8
Pedro Lucas Maia Ramos <pedro.ramos@pagar.me>
2022-04-11 11:27:16 -0300

    Add Private Label Payment Method

---
+++ Mundipagg/Models/Request/CreatePrivateLabelPaymentRequest.cs
@@ -0,0 +1,12 @@
+﻿using Mundipagg.Models.Request;
+
+namespace PagarMe.Models.Request
+{
+    public class CreatePrivateLabelPaymentRequest
+    {
+        public bool Capture { get; set; }
+        public int Installments { get; set; }
+        public string StatementDescriptor { get; set; }
+        public CreateCardRequest Card { get; set; }
+    }
+}
