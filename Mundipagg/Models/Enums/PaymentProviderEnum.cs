43b517d9fde1fb169424b96212449d69f1c66ef2
Andre Postiga Stone <andre.postiga@pagar.me>
2022-08-24 16:47:31 -0300

    feat: include payment provider on CreatePaymentRequest

---
+++ Mundipagg/Models/Enums/PaymentProviderEnum.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.Serialization;
+
+namespace Mundipagg.Models.Enums
+{
+    public enum PaymentProviderEnum
+    {
+        [EnumMember(Value = "mundipagg")]
+        Mundipagg,
+
+        [EnumMember(Value = "pagarme")]
+        Pagarme,
+    }
+}
